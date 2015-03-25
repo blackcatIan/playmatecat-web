ba8403b8dc8b0faa8aa0efeb6a3b5b4c9659d8ce
blackcatIan <blackcatIan@hotmail.com>
2015-03-25 10:38:43 +0800

    1

--- src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
+++ src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
@@ -1,5 +1,18 @@
 package com.playmatecat.project.moduleName;
 
-public class TestCtrl {
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+@RestController
+@RequestMapping("/test")
+public class TestCtrl {
+	
+	@RequestMapping("/testPage")
+	public ModelAndView testPage(Model model) {
+		ModelAndView mav = new ModelAndView("testModule.testMain");
+		model.addAttribute("testParam", "testModelParams");
+		return mav;
+	}
 }
