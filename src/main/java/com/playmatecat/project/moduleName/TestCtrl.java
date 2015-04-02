3e6832c21127d5e592a56e34566edf81a01f37ec
blackcatIan <blackcatIan@hotmail.com>
2015-04-02 15:37:07 +0800

    1

--- src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
+++ src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
@@ -6,11 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("")
 public class TestCtrl {
 	
 	@RequestMapping("/testPage")
 	public ModelAndView testPage(Model model) {
+		ModelAndView mav = new ModelAndView("testModule.testMain");
+		model.addAttribute("testParam", "testModelParams");
+		return mav;
+	}
+	
+	@RequestMapping("/cas-login")
+	public ModelAndView casLogin(Model model) {
+		System.out.println("do-cas-login");
+		
 		ModelAndView mav = new ModelAndView("testModule.testMain");
 		model.addAttribute("testParam", "testModelParams");
 		return mav;
