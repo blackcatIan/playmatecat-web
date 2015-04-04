bf2a4e62befd27b98a8545c73c4696c0588a8e3d
Isa <Isa@Isa-PC>
2015-04-04 17:59:56 +0800

    1

--- src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
+++ src/main/java/com/playmatecat/project/moduleName/TestCtrl.java
@@ -1,14 +1,10 @@
 package com.playmatecat.project.moduleName;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.playmatecat.commons.loginModule.vo.LoginVO;
 import com.playmatecat.utils.encrypt.UtilsAES;
@@ -25,22 +21,24 @@ public class TestCtrl {
 	}
 	
 	@RequestMapping("/cas-login")
-	public ModelAndView casLogin(LoginVO loginVO, Model model) {
+	public ModelAndView casLogin(LoginVO loginVO, Model model, RedirectAttributes redirectAttributes) {
 		System.out.println("do-cas-login");
 		
 		//解密后形式 用户名,密码,单点登录时间
-		String encryptTicket = UtilsAES.encrypt(loginVO.getTicket());
+		String encryptTicket = UtilsAES.decrypt(loginVO.getTicket());
 		
-		String tikectArr[] = encryptTicket.split(",");
-		String username = tikectArr[0];
-		String password = tikectArr[1];
+		String ticketArr[] = encryptTicket.split(",");
+		String username = ticketArr[0];
+		String password = ticketArr[1];
 		
-		Subject subject = SecurityUtils.getSubject();
-		AuthenticationToken token = new UsernamePasswordToken(username,password);
+//		Subject subject = SecurityUtils.getSubject();
+//		AuthenticationToken token = new UsernamePasswordToken(username,password);
 		
 		
-		ModelAndView mav = new ModelAndView("testModule.testMain");
+		ModelAndView mav = new ModelAndView( "redirect:" + loginVO.getUrl());
 		model.addAttribute("testParam", "testModelParams");
+		redirectAttributes.addAttribute("testParam", "testModelParams");
+		
 		return mav;
 	}
 }
